3be20cc963176507a8ae1fa1e7dc32ed2ac8fc32
batsaike <batsa2010isaac@gmail.com>
2023-02-21 14:29:11 -0800

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
 /**
- * time_teble - print a multiplication table of single-digit numbers
- *
- * Return: void
+ * times_table - prints the 9 times table, starting with 0
  */
-void time_teble(void)
+void time_table(void)
 {
-	int i;
-	int j;
-
-	for (i = 0; i < 10; ++i)
-	{
-		for (j = 0; j < 10; ++j)
-		{
-			if (j == 0)
-				;
-			else if ((i * j) < 10)
-				_putchar(' ');
-			else
-				_putchar('0' + i * j / 10);
-
-			_putchar('0' + i * j % 10);
-
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
+int i, j, k;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+k = j * i;
+if (j == 0)
+{
+_putchar(k + '0');
+}
+if (k < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
